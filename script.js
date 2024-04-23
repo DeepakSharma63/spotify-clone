d57046ec2d447d9df5b3b4ac2ae56b91f94d2250
DeepakSharma63 <deepsharna2501447@gmail.com>
2024-04-23 22:07:34 +0530

    Fixed URL

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+  let a = await fetch(`https://spotify-clone-steel.vercel.app/${folder}/`)
   let response = await a.text();
   let div = document.createElement("div")
   div.innerHTML = response;
@@ -76,7 +76,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:3000/songs/`)
+  let a = await fetch(`https://spotify-clone-steel.vercel.app/songs/`)
   let response = await a.text();
   let div = document.createElement("div")
   div.innerHTML = response;
@@ -91,7 +91,7 @@ async function displayAlbums() {
       let folder = e.href.split("/").splice(3)[1]
 
       //Get the meta data of the folder
-      let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+      let a = await fetch(`https://spotify-clone-steel.vercel.app/songs/${folder}/info.json`)
       let response = await a.json();
       console.log(response)
       cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
